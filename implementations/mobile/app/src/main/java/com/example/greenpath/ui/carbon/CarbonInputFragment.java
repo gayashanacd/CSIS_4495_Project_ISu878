bb5d7b055d9baf536c2ba28ac9d4e364c6ccf0fb
Gayashan <gayazhanmit@yahoo.com>
2024-11-30 03:28:57 -0800

    latest in carbon entry

--- implementations/mobile/app/src/main/java/com/example/greenpath/ui/carbon/CarbonInputFragment.java
+++ implementations/mobile/app/src/main/java/com/example/greenpath/ui/carbon/CarbonInputFragment.java
@@ -11,9 +11,12 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.room.Room;
 
+import com.android.volley.RequestQueue;
 import com.example.greenpath.databases.GreenPathDatabase;
 import com.example.greenpath.databinding.FragmentCarbonBinding;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -22,7 +25,10 @@ public class CarbonInputFragment extends Fragment {
 
     private FragmentCarbonBinding binding;
     GreenPathDatabase cmDB;
+    private RequestQueue requestQueue;
     ArrayList barArraylist;
+    String mongoId, formattedDate;
+    final String API = "http://10.0.2.2:5000/api/";
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -37,6 +43,11 @@ public class CarbonInputFragment extends Fragment {
 
         SharedPreferences settings =  requireActivity().getSharedPreferences("PREFS_CM", 0);
         String userId = settings.getString("USERID", "");
+        mongoId = settings.getString("MONGOID", "");
+        LocalDate currentDate = LocalDate.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        formattedDate = currentDate.format(formatter);
+        binding.editTextDateCarbonInput.setText(formattedDate);
 
         executorService.execute(new Runnable() {
             @Override
